a5090b62e23489df3f2ee64391af6b5b11872d4a
Davide Capomagi <davide.capomagi@gmail.com>
2022-05-12 07:27:39 +0100

    Testing thread channel

--- forward_discard/src/main.rs
+++ forward_discard/src/main.rs
@@ -1,3 +1,17 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        for i in 1..10 {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    while let Some(val) = rx.recv().ok() {
+        println!("hi number {} from the main thread!", val);
+    }
 }
