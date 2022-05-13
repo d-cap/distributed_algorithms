0cd0d414d1b29525626dbb8c178609b3c2302fc9
Davide Capomagi <davide.capomagi@gmail.com>
2022-05-13 07:55:44 +0100

    Created ring of threads

--- forward_discard/src/main.rs
+++ forward_discard/src/main.rs
@@ -1,17 +1,43 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
+use std::sync::mpsc::{self, Receiver, Sender};
+
+struct Ring<T> {
+    nodes: Vec<Node<T>>,
+}
+
+struct Node<T> {
+    tx_left: Sender<T>,
+    rx_left: Receiver<T>,
+    tx_right: Sender<T>,
+    rx_right: Receiver<T>,
+}
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    thread::spawn(move || {
-        for i in 1..10 {
-            tx.send(i).unwrap();
-            thread::sleep(Duration::from_millis(100));
-        }
-    });
+    let size = 10;
+    let mut ring: Ring<&str> = Ring {
+        nodes: Vec::with_capacity(size),
+    };
 
-    while let Some(val) = rx.recv().ok() {
-        println!("hi number {} from the main thread!", val);
+    let (junction_tx_left, junction_rx_right) = mpsc::channel();
+    let (junction_tx_right, junction_rx_left) = mpsc::channel();
+    let mut previous_right_tx = junction_tx_right;
+    let mut previous_right_rx = junction_rx_right;
+
+    for i in 0..size {
+        let (tx_left, rx_right) = mpsc::channel();
+        let (tx_right, rx_left) = mpsc::channel();
+        ring.nodes.push(Node {
+            tx_left,
+            rx_left,
+            tx_right: previous_right_tx,
+            rx_right: previous_right_rx,
+        });
+
+        if i + 1 == size - 1 {
+            previous_right_tx = junction_tx_left;
+            previous_right_rx = junction_rx_left;
+        } else {
+            previous_right_tx = tx_right;
+            previous_right_rx = rx_right;
+        }
     }
 }
